1852eee3145b80a80b0f4ce3e54e55de9c626ffa
Itsuki Kuwahara <248c117c@stu.kobe-u.ac.jp>
2025-09-02 16:44:12 +0900

    少し大きなモデルに変更

--- config.py
+++ config.py
@@ -286,7 +286,7 @@ LLAMA3_TEST_CONFIG = ExperimentConfig(
     model=ModelConfig(
         name="meta-llama/Llama-3.2-1B",
         sae_release="seonglae/Llama-3.2-1B-sae",
-        sae_id="Llama-3.2-1B_blocks.12.hook_resid_pre_14336_topk_48_0.0002_42_faithful-llama3.2-1b_512", 
+        sae_id="Llama-3.2-1B_blocks.12.hook_resid_pre_14336_topk_48_0.0002_42_faithful-llama3.2-3b_512", 
         device="auto"
     ),
     data=DataConfig(sample_size=5),
@@ -318,7 +318,7 @@ SERVER_LARGE_CONFIG = ExperimentConfig(
     model=ModelConfig(
         name="meta-llama/Llama-3.2-1B",
         sae_release="seonglae/Llama-3.2-1B-sae",
-        sae_id="Llama-3.2-1B_blocks.12.hook_resid_pre_14336_topk_48_0.0002_42_faithful-llama3.2-1b_512", 
+        sae_id="Llama-3.2-1B_blocks.12.hook_resid_pre_14336_topk_48_0.0002_42_faithful-llama3.2-3b_512", 
         device="auto"
     ),
     data=DataConfig(sample_size=1000),
